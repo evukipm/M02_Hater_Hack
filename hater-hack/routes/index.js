3788b914b4d2159f3ede53304c3f28c288a77600
Eva <eva.p.mejias@gmail.com>
2018-10-21 11:45:08 +0200

    permisos en ruta principal

--- hater-hack/routes/index.js
+++ hater-hack/routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  if (req.locals.currentUser) {
+    res.render('index');
+  } else {
+    res.redirect('/auth/login');
+  }
 });
 
 module.exports = router;
