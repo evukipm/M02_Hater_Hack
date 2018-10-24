abb84d01ea39b250ca796f37e60df6a12a03c397
Evukipm <eva.p.mejias@gmail.com>
2018-10-24 13:19:24 +0200

    upload images done

--- routes/profile.js
+++ routes/profile.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require('../models/hater');
 const Post = require('../models/crypost');
 const middlewares = require('../middleware/middlewares');
+const uploadCloud = require('../services/cloudinary.js');
 
 router.get('/:id', (req, res, next) => {
   const id = req.params.id;
@@ -14,6 +15,31 @@ router.get('/:id', (req, res, next) => {
           res.render('profile/profile', { user, post });
         })
         .catch(next);
+    })
+    .catch(next);
+});
+
+router.get('/:id/edit', (req, res, next) => {
+  res.render('profile/edit');
+});
+
+router.post('/:id/edit', uploadCloud.single('avatar'), (req, res, next) => {
+  const id = req.params.id;
+  const name = req.body.username;
+  const description = req.body.description;
+  const cohort = req.body.cohort;
+  const campus = req.body.campus;
+  const avatar = req.file.url;
+
+  User.findById(id)
+    .then(user => {
+      user.avatar = avatar;
+      user.username = name;
+      user.description = description;
+      user.cohort = cohort;
+      user.campus = campus;
+      user.save();
+      res.redirect(`/profile/${id}`);
     })
     .catch(next);
 });
