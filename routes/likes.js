5337fae9f2d9962f40ca32a79d2b9058f90afe73
Evukipm <eva.p.mejias@gmail.com>
2018-10-25 15:17:44 +0200

    likes done

--- routes/likes.js
+++ routes/likes.js
@@ -2,15 +2,32 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/crypost');
 
-router.get('/likes', (req, res, next) => {
-  Post.find()
+router.post('/likes', (req, res, next) => {
+  const postId = req.body.postId;
+  const value = req.body.button;
+
+  Post.findById(postId)
     .then(post => {
-      res.status(200);
-      res.json(post);
+      if (value === 'a') {
+        post.hateButtons.buttonA++;
+        post.save();
+        res.json(post.hateButtons.buttonA);
+      } else if (value === 'b') {
+        post.hateButtons.buttonB++;
+        post.save();
+        res.json(post.hateButtons.buttonB);
+      } else if (value === 'c') {
+        post.hateButtons.buttonC++;
+        post.save();
+        res.json(post.hateButtons.buttonC);
+      } else if (value === 'd') {
+        post.hateButtons.buttonD++;
+        post.save();
+        res.json(post.hateButtons.buttonD);
+      }
     })
     .catch(error => {
-      res.status(500);
-      res.json(error);
+      console.log(error);
     });
 });
 
