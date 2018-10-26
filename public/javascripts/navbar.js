76a6f9a00f798409e558549add608dd8befa2ce4
Iban Dasca <ibandasca@MacBook-Pro-de-Iban.local>
2018-10-26 11:15:08 +0200

    css

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,14 +1,17 @@
-let openButton = document.querySelector('div.menu div.nav-elements');
+let openButton = document.querySelector('i.fa-bars');
 let navbar = document.querySelector('div.navbar');
-let closeButton = document.querySelector('div.fas div.fa-sort-up');
+let closeButton = document.querySelector('i.fa-sort-up');
+let mainContainer = document.querySelector('div.main-container');
 
 openButton.addEventListener('click', openNav);
 closeButton.addEventListener('click', closeNav);
 
 function openNav () {
   navbar.classList.add('open');
+  mainContainer.classList.add('move');
 }
 
 function closeNav () {
   navbar.classList.remove('open');
+  mainContainer.classList.remove('move');
 }
